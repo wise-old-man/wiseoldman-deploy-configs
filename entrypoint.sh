5b85f528c2ec35b5d0616c012db2c4e22a8210c9
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-07-02 23:53:58 +0100

    Try installing docker (#9)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo docker login -u $1 -p $2
+docker login -u $1 -p $2
 docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
 docker push wiseoldman/nginx-deploy-config:latest
