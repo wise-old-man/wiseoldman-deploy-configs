106634dc491716963fa7718ac832bf4cadf60d99
Psikoi <ruben.amendoeira@gmail.com>
2020-07-02 22:41:09 +0100

    Test secret-to-args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/sh -l
 
 echo "Hello $1"
+
+echo "Test username $2"
+
+#docker login -u ${{ secrets.DOCKER_USERNAME }} -p ${{ secrets.DOCKER_PASSWORD }}
+    
+#docker build . --file Dockerfile --tag wiseoldman/nginx-deploy-config:latest
+#docker push wiseoldman/nginx-deploy-config:latest
