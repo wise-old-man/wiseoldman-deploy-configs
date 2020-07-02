ce7843b3c8d74d1ead8d276683ed891ed64f661f
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-07-02 23:38:03 +0100

    Sigh (#7)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
-#!/bin/sh -l
+#!/bin/sh
 
-docker login -u $1 -p $2
-docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
-docker push wiseoldman/nginx-deploy-config:latest
+sh "docker login -u $1 -p $2"
+# docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
+# docker push wiseoldman/nginx-deploy-config:latest
