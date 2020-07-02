38e5d1563b4b997cadbfc0fad57ed1abb71733ca
Psikoi <ruben.amendoeira@gmail.com>
2020-07-02 23:46:22 +0100

    Trying anything at this point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sh "docker login -u $1 -p $2"
-# docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
-# docker push wiseoldman/nginx-deploy-config:latest
+sudo docker login -u $1 -p $2
+docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
+docker push wiseoldman/nginx-deploy-config:latest
