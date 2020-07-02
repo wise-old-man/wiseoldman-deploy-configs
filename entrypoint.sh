9009d01302f29287508ffd55a9d179c94f295d40
Ruben Amendoeira <ruben.amendoeira@gmail.com>
2020-07-03 00:04:43 +0100

    Cd into folder pls work (#10)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd nginx
+
 docker login -u $1 -p $2
-docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
+docker build . --file Dockerfile --tag wiseoldman/nginx-deploy-config:latest
 docker push wiseoldman/nginx-deploy-config:latest
