de36e641ced98b7ad727baa00cff41f5f71d29b4
Psikoi <ruben.amendoeira@gmail.com>
2020-07-02 22:56:22 +0100

    Add nginx image building

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-
-echo "Test username $2"
-
-#docker login -u ${{ secrets.DOCKER_USERNAME }} -p ${{ secrets.DOCKER_PASSWORD }}
-    
-#docker build . --file Dockerfile --tag wiseoldman/nginx-deploy-config:latest
-#docker push wiseoldman/nginx-deploy-config:latest
+docker login -u $1 -p $2
+docker build . --file nginx/Dockerfile --tag wiseoldman/nginx-deploy-config:latest
+docker push wiseoldman/nginx-deploy-config:latest
